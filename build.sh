f342fde7d938954a50ec1acad8d7e27547f9bef7
Florian Weber <florian@webflo.org>
2016-02-10 00:13:53 +0100

    Add sperate checkout script

--- build.sh
+++ build.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/env sh
-mkdir -p tmp
-
-if [ ! -d "tmp/drupal" ]; then
-  git clone --branch 8.0.x https://git.drupal.org/project/drupal.git tmp/drupal
-fi
-
-if [ ! -d "tmp/metapackage" ]; then
-  git clone --branch master https://github.com/webflo/drupal-core-strict.git tmp/metapackage
-fi
-
 php build.php
 cd tmp/metapackage
 git push --all origin
